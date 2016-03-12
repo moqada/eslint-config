5b912922192b9c6aff2cfd37d90478b541966d6f
moqada <moqada@gmail.com>
2016-03-12 19:41:31 +0900

    refactor(react-native): Add env and disable split-platform-components

--- react-native.js
+++ react-native.js
@@ -3,6 +3,10 @@ var react = require('./react');
 
 module.exports = extend(true, {}, react, {
   'plugins': ['react', 'react-native'],
+  'env': {
+    'browser': true,
+    'node': true
+  },
   'globals': {
     '__DEV__': true
   },
@@ -10,8 +14,8 @@ module.exports = extend(true, {}, react, {
     // 未利用のStyleを警告
     // https://github.com/intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-unused-styles.md
     'react-native/no-unused-styles': 2,
-    // Android/iOS用のコンポーネントの分離を推奨
+    // Android/iOS用のコンポーネントのファイル分離を推奨 (x.ios.js, x.android.js)
     // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/split-platform-components.md
-    'react-native/split-platform-components': 2
+    'react-native/split-platform-components': 0  // 片方のみの場合は不要
   }
 });
