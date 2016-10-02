c0187f43158cd4f7c0949bddea12739420de86c4
moqada <moqada@gmail.com>
2016-10-02 20:32:17 +0900

    feat(react): Update rules for eslint-plugin-react 6.3.x

--- react.js
+++ react.js
@@ -31,6 +31,9 @@ module.exports = {
     // displayName属性有無
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
     'react/display-name': [2, {'ignoreTranspilerName': false}],
+    // Reactコンポーネントに特定のpropsを禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-component-props.md
+    'react/forbid-component-props': 2,  // className, styleを禁止
     // 曖昧な PropTypes 禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
     'react/forbid-prop-types': 1,  // きつそうなので警告まで
@@ -70,9 +73,15 @@ module.exports = {
     // propsでの.bind() or arrow function禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
     'react/jsx-no-bind': 0,  // flowの都合上bindつかってる
+    // JSX内TextNodeコメント禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-comment-textnodes.md
+    'react/jsx-no-comment-textnodes': 2,
     // props の重複禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
     'react/jsx-no-duplicate-props': 2,
+    // findDOMNodeの使用禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-find-dom-node.md
+    'react/no-find-dom-node': 2,
     // target='_blank'の禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md
     'react/jsx-no-target-blank': 2,
@@ -82,30 +91,33 @@ module.exports = {
     // コンポーネント名にパスカルケースの使用を強制
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
     'react/jsx-pascal-case': 2,
-    // props の使用をアルファベット順に制限
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
-    'react/jsx-sort-props': [2, {'callbacksLast': true, 'shorthandFirst': true}],  // 値なしは最初、callback(onXXX)系は最後に
     // React 的に不適切な箇所での no-unused-vars 発動禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-react.md
     'react/jsx-uses-react': 2,
     // JSX 内で使用中の変数への no-unused-vars 発動禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-vars.md
     'react/jsx-uses-vars': 2,
-    // JSX内TextNodeコメント禁止
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-comment-textnodes.md
-    'react/no-comment-textnodes': 2,
+    // 括弧のない複数行の JSX を禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-wrap-multilines.md
+    'react/jsx-wrap-multilines': 2,
+    // children propsを禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-children-prop.md
+    'react/no-children-prop': 2,  // flowの関係上使いたい場合がある
     // dangerouslySetInnerHTML の禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
     'react/no-danger': 0,  // 使うときは覚悟を持って使う
+    // childrenとdangerouslySetInnerHTMLの同時使用を禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
+    'react/no-danger-with-children': 2,
     // deprecatedな記法を警告
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
     'react/no-deprecated': 0,  // 必要なさそう
     // componentDidMount 内での setState 禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-mount-set-state.md
-    'react/no-did-mount-set-state': [2, 'allow-in-func'],
+    'react/no-did-mount-set-state': 2,
     // componentDidUpdate 内での setState 禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-update-set-state.md
-    'react/no-did-update-set-state': [2, 'allow-in-func'],
+    'react/no-did-update-set-state': 2,
     // state の直接更新禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
     'react/no-direct-mutation-state': 2,
@@ -124,18 +136,21 @@ module.exports = {
     // 文字列refの禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-string-refs.md
     'react/no-string-refs': 2,
+    // escapeしていないmarkupを禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
+    'react/no-unescaped-entities': 2,
     // 不適切な DOM 属性使用の禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
     'react/no-unknown-property': 2,
+    // 使用していないpropsを禁止
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
+    'react/no-unused-prop-types': 2,
     // propTypes 未定義の props 使用禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
     'react/prop-types': 2,
     // JSX スコープ内の React 定義を必須化
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
     'react/react-in-jsx-scope': 2,
-    // require で拡張子を強制
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-extension.md
-    'react/require-extension': 0,
     // shouldComponentUpdateを強制
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-optimization.md
     'react/require-optimization': 0,
@@ -150,7 +165,7 @@ module.exports = {
     'react/prefer-stateless-function': 2,
     // コンポーネントの不要な閉じタグを禁止
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
-    'react/self-closing-comp': 2,
+    'react/self-closing-comp': [2, {'component': true, 'html': true}],
     // 閉じタグ前に空白を強制
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-space-before-closing.md
     'react/jsx-space-before-closing': [2, 'always'],
@@ -160,6 +175,7 @@ module.exports = {
       2,
       {
         'order': [
+          'type-annotations',
           'static-methods',
           'lifecycle',
           'everything-else',
@@ -199,8 +215,8 @@ module.exports = {
     // propTypes の定義をアルファベット順に制限
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-prop-types.md
     'react/sort-prop-types': 2,
-    // 括弧のない複数行の JSX を禁止
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
-    'react/wrap-multilines': 2
+    // style propsをObjectに強制
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/style-prop-object.md
+    'react/style-prop-object': 2
   }
 };
