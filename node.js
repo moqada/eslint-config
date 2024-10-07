e7e4e18b2214f841c21a3cdee8ada6a462ab89b0
moqada <moqada@gmail.com>
2024-10-08 04:07:58 +0900

    chore: Update node.js
    - 原則ルールをそのままに Flat config に移行
    - eslint-plugin-node -> eslint-plugin-n に置き換え
    - no-unsupported-features は no-unsupported-features/node-builtins と no-unsupported-features/es-syntax に変更
      - https://github.com/mysticatea/eslint-plugin-node/blob/8ef07af5711b74199745a619c6f522e1e32acfaf/docs/rules/no-unsupported-features.md?plain=1#L2-L3

--- node.js
+++ node.js
@@ -1,41 +1,47 @@
-module.exports = {
-  'env': {
-    'node': true
-  },
-  'plugins': ['node'],
-  'rules': {
-    // exportsスタイル強制
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/exports-style.md
-    'node/exports-style': [2, 'module.exports'],
-    // depreactedなAPIを警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-deprecated-api.md
-    'node/no-deprecated-api': 2,
-    // package.jsonにないモジュールのimportを禁止
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-extraneous-import.md
-    'node/no-extraneous-import': 2,
-    // package.jsonにないモジュールのrequireを禁止
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-extraneous-require.md
-    'node/no-extraneous-require': 2,
-    // import先が存在しない場合警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-missing-import.md
-    'node/no-missing-import': 2,
-    // require先が存在しない場合警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-missing-require.md
-    'node/no-missing-require': 2,
-    // binがpackage.jsonのfilesにない場合に警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unpublished-bin.md
-    'node/no-unpublished-bin': 2,
-    // publishされないモジュールをimportした場合警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unpublished-import.md
-    'node/no-unpublished-import': 2,
-    // publishされないモジュールをrequireeした場合警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unpublished-require.md
-    'node/no-unpublished-require': 2,
-    // 当該Node.jsバージョンで未サポートの記法を警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features.md
-    'node/no-unsupported-features': 2,
-    // shebangが不正に利用されていた場合警告
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/shebang.md
-    'node/shebang': 2
+import globals from 'globals';
+import nPlugin from 'eslint-plugin-n';
+
+export default [
+  {
+    languageOptions: {globals: globals.node},
+    plugins: {n: nPlugin},
+    rules: {
+      // exportsスタイル強制
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/exports-style.md
+      'n/exports-style': [2, 'module.exports'],
+      // depreactedなAPIを警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-deprecated-api.md
+      'n/no-deprecated-api': 2,
+      // package.jsonにないモジュールのimportを禁止
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-extraneous-import.md
+      'n/no-extraneous-import': 2,
+      // package.jsonにないモジュールのrequireを禁止
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-extraneous-require.md
+      'n/no-extraneous-require': 2,
+      // import先が存在しない場合警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-missing-import.md
+      'n/no-missing-import': 2,
+      // require先が存在しない場合警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-missing-require.md
+      'n/no-missing-require': 2,
+      // binがpackage.jsonのfilesにない場合に警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unpublished-bin.md
+      'n/no-unpublished-bin': 2,
+      // publishされないモジュールをimportした場合警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unpublished-import.md
+      'n/no-unpublished-import': 2,
+      // publishされないモジュールをrequireeした場合警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unpublished-require.md
+      'n/no-unpublished-require': 2,
+      // 当該Node.jsバージョンで未サポートのSyntaxを警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unsupported-features/es-syntax.md
+      'n/no-unsupported-features/es-syntax': 2,
+      // 当該Node.jsバージョンで未サポートの組み込みAPIを警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unsupported-features/node-builtins.md
+      'n/no-unsupported-features/node-builtins': 2,
+      // shebangが不正に利用されていた場合警告
+      // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/shebang.md
+      'n/shebang': 2
+    }
   }
-};
+];
