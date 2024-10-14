e3d6a90201521dcbfe8cc67f22fbd932678007a2
moqada <moqada@gmail.com>
2024-10-14 19:29:55 +0900

    chore: Adjust rules for jsdoc
    - 本家の valid-jsdoc, require-jsdoc の廃止に伴い、
      元々 valid-jsdoc を模倣して一部 FlowType 用に改変していた flow-jsdoc.js のルールを index.js に移動した
      - index.js のほうは型情報がない前提なので flow-jsdoc.js で無効化していた以下のルールを有効にしている
        - jsdoc/check-types, jsdoc/require-param-type
      - 反面、型情報が前提の flow-jsdoc.js では上記のルールを上書きして無効化するようにそのまま残している

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+
 export default [
   {
+    plugins: {jsdoc: jsdocPlugin},
     rules: {
       /**
        * Possible Errors
@@ -833,7 +836,63 @@ export default [
       'no-bitwise': 2,
       // ++ / -- 禁止
       // http://eslint.org/docs/rules/no-plusplus
-      'no-plusplus': 0
+      'no-plusplus': 0,
+
+      /**
+       * JSDoc
+       */
+      // https://github.com/gajus/eslint-plugin-jsdoc#check-param-names
+      // @paramと引数名の一致を確認
+      'jsdoc/check-param-names': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#check-tag-names
+      // タグの名前(@param, @returns, etc...)が正しいか確認
+      'jsdoc/check-tag-names': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#check-types
+      // 型名(number, string, etc...)が正しいか確認
+      'jsdoc/check-types': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-description-complete-sentence
+      // descriptionを文章形式にすることを強制
+      'jsdoc/require-description-complete-sentence': 0,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-example
+      // @exampleを強制
+      'jsdoc/require-example': 0,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-hyphen-before-param-description
+      // @paramの説明の前にハイフンを強制
+      'jsdoc/require-hyphen-before-param-description': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-param
+      // @paramを強制
+      'jsdoc/require-param': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-param-description
+      // @paramにdescriptionを強制
+      'jsdoc/require-param-description': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-param-name
+      // @paramにnameを強制
+      'jsdoc/require-param-name': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-param-type
+      // @paramにtypeを強制
+      'jsdoc/require-param-type': 2,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-returns-description
+      // @returnsにdescriptionを強制
+      'jsdoc/require-returns-description': 0,
+      // https://github.com/gajus/eslint-plugin-jsdoc#require-returns-type
+      // @returnsにtypeを強制
+      'jsdoc/require-returns-type': 0,
+      // https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/tag-lines.md
+      // タグ間の空行スタイル
+      'jsdoc/tag-lines': [
+        2,
+        'never',
+        {
+          // 最初のタグの前に空行を入れる
+          startLines: 1
+        }
+      ]
+    },
+    settings: {
+      jsdoc: {
+        // タグ名のエイリアスを定義
+        tagNamePreference: {returns: 'return'}
+      }
     }
   }
 ];
